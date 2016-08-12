299d140a570038376669e55e56a3d3f9e578db9c
ROSSKO\pavel.krizhanovskiy <pavel.krizhanovskiy@rossko.ru>
2016-08-12 12:13:01 +0700

    Сделана ещё одна часть 2-го задания Улучшен readme

---
+++ src/main/java/com/norg/home02/shuffleusage/ShuffleDemo.java
@@ -0,0 +1,7 @@
+package com.norg.home02.ShuffleUsage;
+
+/**
+ * Created by pavel.krizhanovskiy on 12.08.2016.
+ */
+public class ShuffleDemo {
+}
---
+++ src/main/java/com/norg/home02/shuffleusage/cards/Card.java
@@ -0,0 +1,7 @@
+package com.norg.home02.shuffleusage.cards;
+
+/**
+ * Created by pavel.krizhanovskiy on 12.08.2016.
+ */
+public class Card {
+}
