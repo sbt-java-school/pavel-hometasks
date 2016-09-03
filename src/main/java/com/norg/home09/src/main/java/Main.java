e9303a28e9efad4213966772a4f081196e6a3f03
SonicNorg <pavel.proger@gmail.com>
2016-09-03 11:09:23 +0700

    home09

---
+++ src/main/java/com/norg/home09/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+/**
+ * Created by Norg on 03.09.2016.
+ */
+public class Main {
+
+}
---
+++ src/main/java/com/norg/home09/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by Norg on 03.09.2016.
+ */
+public class MainTest {
+
+
+
+}
