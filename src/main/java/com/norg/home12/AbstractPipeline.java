b76df6c11140445396fa7dd1d4b9ac4e12151cc3
SonicNorg <pavel.proger@gmail.com>
2016-09-24 15:44:21 +0700

    работы по заданию 12 (Стрим) написал filter

--- src/main/java/com/norg/home12/AbstractPipeline.java
+++ src/main/java/com/norg/home12/AbstractPipeline.java
@@ -1,23 +1,44 @@
 package com.norg.home12;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Created by pavel.krizhanovskiy on 19.09.2016.
  */
-public abstract class AbstractPipeline<P_IN, E_OUT> {
-    private List source;
+public abstract class AbstractPipeline {
+    private List<? extends Consumer> source;
 
     protected AbstractPipeline previousStage;
+    protected AbstractPipeline firstStage;
 
-    protected AbstractPipeline(List<E_OUT> source) {
+    protected AbstractPipeline(List<? extends Consumer> source) {
         this.source = source;
-        this.previousStage = null;
+        previousStage = null;
+        firstStage = this;
     }
 
-    protected AbstractPipeline(AbstractPipeline<?, P_IN> previous) {
+    protected AbstractPipeline(AbstractPipeline previous) {
         this.previousStage = previous;
         this.source = null;
     }
 
+    final Consumer processWrap(Consumer wrap) {
+        AbstractPipeline p = this;
+        while (p.previousStage != null) {
+            wrap = p.unwrap(wrap);
+            p = p.previousStage;
+        }
+        return wrap;
+    }
+
+    final void acceptAll(Consumer wrap) {
+        List<? extends Consumer> list = firstStage.source;
+        for (Consumer consumer:
+             list) {
+            wrap.accept(consumer);
+        }
+    }
+
+    abstract Consumer unwrap(Consumer wrap);
 }
--- src/main/java/com/norg/home12/Streams.java
+++ src/main/java/com/norg/home12/Streams.java
@@ -2,17 +2,18 @@ package com.norg.home12;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 /**
  * Created by pavel.krizhanovskiy on 19.09.2016.
  */
-public class Streams<P_IN, E_OUT> extends AbstractPipeline {
+public class Streams extends AbstractPipeline {
     protected Streams(List source) {
         super(source);
     }
 
-    public Streams(Streams<P_IN, E_OUT> streams) {
+    public Streams(Streams streams) {
         super(streams);
     }
 
@@ -22,7 +23,16 @@ public class Streams<P_IN, E_OUT> extends AbstractPipeline {
 
     @SuppressWarnings("unchecked")
     public Streams filter(Predicate predicate) {
-        return new Streams(this);
+        return new Streams(this) {
+            @Override
+            Consumer unwrap(Consumer wrap) {
+                return (o) -> {
+                    if(predicate.test(o)) {
+                        wrap.accept(o);
+                    }
+                };
+            }
+        };
     }
 
     public Streams transform() {
@@ -32,5 +42,10 @@ public class Streams<P_IN, E_OUT> extends AbstractPipeline {
 
     public Map toMap() {
         return null;
+    }
+
+    @Override
+    Consumer unwrap(Consumer wrap) {
+        throw new IllegalStateException();
     }
 }
