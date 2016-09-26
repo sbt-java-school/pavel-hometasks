a1ab641a59d7facf5907ead2a4bed822bc2bb07c
ROSSKO\pavel.krizhanovskiy <pavel.krizhanovskiy@rossko.ru>
2016-09-26 12:13:33 +0700

    смерджил

--- src/main/java/com/norg/home12/streams/AbstractPipeline.java
+++ src/main/java/com/norg/home12/streams/AbstractPipeline.java
@@ -1,26 +1,44 @@
 package com.norg.home12.streams;
 
-import java.util.function.Supplier;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Created by pavel.krizhanovskiy on 19.09.2016.
  */
-public class AbstractPipeline {
-    private final AbstractPipeline sourceStage;
-    private Supplier<?> sourceSupplier;
-    private boolean closed;
+public abstract class AbstractPipeline {
+    private List<? extends Consumer> source;
 
-    private final AbstractPipeline previousStage;
-    private AbstractPipeline nextStage;
+    protected AbstractPipeline previousStage;
+    protected AbstractPipeline firstStage;
 
-    private int depth;
-
-    AbstractPipeline(Supplier<?> source,
-                     int sourceFlags, boolean parallel) {
-        this.previousStage = null;
-        this.sourceSupplier = source;
-        this.sourceStage = this;
-        this.depth = 0;
-        closed = false;
+    protected AbstractPipeline(List<? extends Consumer> source) {
+        this.source = source;
+        previousStage = null;
+        firstStage = this;
     }
+
+    protected AbstractPipeline(AbstractPipeline previous) {
+        this.previousStage = previous;
+        this.source = null;
+    }
+
+    final Consumer processWrap(Consumer wrap) {
+        AbstractPipeline p = this;
+        while (p.previousStage != null) {
+            wrap = p.unwrap(wrap);
+            p = p.previousStage;
+        }
+        return wrap;
+    }
+
+    final void acceptAll(Consumer wrap) {
+        List<? extends Consumer> list = firstStage.source;
+        for (Consumer consumer:
+             list) {
+            wrap.accept(consumer);
+        }
+    }
+
+    abstract Consumer unwrap(Consumer wrap);
 }
